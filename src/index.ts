33bdddd5429b6060a733ae7ec4c04e19e262481a
Stephen Lautier <stephen.lautier@outlook.com>
2026-03-18 12:09:03 +0100

    fmt + settings

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import * as core from "@actions/core"
 import { run } from "./main"
 
 run().catch(error => {
-	const message = error instanceof Error
-		? error.message
-		: String(error ?? "Unknown error")
+	const message = error instanceof Error ? error.message : String(error ?? "Unknown error")
 	core.setFailed(message)
-});
+})
